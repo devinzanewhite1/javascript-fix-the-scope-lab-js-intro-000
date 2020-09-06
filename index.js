b5e4a413956a1fef8f07bdb88a0d943b00130765
confident-panel-8659 <devinzanewhite@gmail.com>
2020-09-06 00:41:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ function add2(n) {
   return parseInt(n, 10) + two
 
 }
-
 var funkyFunction = function() {
   return function() {
     return "FUNKY!"
@@ -28,4 +27,8 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+function theFunk()
+{
+  var theFunk() = "FUNKY"
+  return theFunk()
+}
